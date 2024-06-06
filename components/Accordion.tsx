e84f1a4de5b5d1d4808dfebdb739045dca72a7fe
Rahul Tibrewal <tibrewalrahul01@gmail.com>
2024-06-06 12:23:13 +0530

    css fixes

--- components/Accordion.tsx
+++ components/Accordion.tsx
@@ -9,19 +9,18 @@ const Accordion = () => {
       <section key={index} onClick={() => setSelected(index)}>
         <article className="rounded p-2 pb-2 ">
           <div className="relative h-[45px] cursor-pointer" >
-            {selected === index && <Image src={AppImage.bg} alt="background-image" layout="fill" />}
-            <div className="container mx-auto max-w-[1237px] px-5 h-[48px] flex justify-between items-center relative">
+            {selected === index && <Image src={AppImage.bg} alt="background-image" layout="fill" objectFit='cover' />}
+            <div className="container mx-auto lg:max-w-6xl px-5 h-[48px] flex justify-between items-center relative">
               <p className="text-white uppercase lg:text-[1.5rem] md:text-[1rem] xs:text-[1rem] font-medium">
                 Lorem ipsum dolor sit amet
               </p>
               <button className="text-left text-white font-semibold focus:outline-none">
                 <span className='text-2xl'>{selected === index ? " - " : "+"}</span>
-                {/* <Image alt="plus-icon" src={tab === index ? Icon.plus : Icon.minus} /> */}
               </button>
             </div>
           </div>
 
-          {selected === index && <p className="container mx-auto max-w-[1237px] px-5  leading-7 mt-5 text-[#a1a1a1]">
+          {selected === index && <p className="container mx-auto lg:max-w-6xl px-5  leading-7 mt-5 text-[#a1a1a1]">
             {`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`}
           </p>}
         </article>
--- pages/index.tsx
+++ pages/index.tsx
@@ -16,8 +16,8 @@ export default function Home() {
       <Header />
       <main>
         <section className="mx-auto px-5 pt-5">
-          <article className="container mx-auto lg:max-w-7xl lg:pt-10 sm:pt-0">
-            <h1 className="mb-4 text-4xl font-bold text-white md:text-7xl lg:text-[7.5rem] dark:text-white">
+          <article className="container mx-auto lg:max-w-6xl lg:pt-10 sm:pt-0">
+            <h1 className="mb-4 text-4xl font-bold text-white md:text-6xl lg:text-[7.5rem] dark:text-white">
               LOREM IPSUM
             </h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-8 mt-12">
@@ -26,7 +26,7 @@ export default function Home() {
               </div>)}
             </div>
           </article>
-          <article className="container mx-auto lg:max-w-7xl lg:pt-10 sm:pt-0 lg:mt-24 py-10 mb-[10rem] lg:block md:hidden sm:hidden max-sm:hidden relative backgroundContainer cursor-pointer" id="about">
+          <article className="container mx-auto lg:max-w-6xl lg:pt-10 sm:pt-0 lg:mt-24 py-10 mb-[10rem] lg:block md:hidden sm:hidden max-sm:hidden relative backgroundContainer cursor-pointer" id="about">
             <AboutUs />
           </article>
         </section>
@@ -34,8 +34,8 @@ export default function Home() {
           <AdBanner />
         </section>
         <section className="mx-auto px-5 pt-5">
-          <article className="container mx-auto lg:max-w-7xl lg:pt-10 sm:pt-0">
-            <h1 className="mb-4 text-4xl font-bold text-white md:text-7xl lg:text-[7.5rem] dark:text-white">
+          <article className="container mx-auto lg:max-w-6xl lg:pt-10 sm:pt-0">
+            <h1 className="mb-4 text-4xl font-bold text-white md:text-6xl lg:text-[7.5rem] dark:text-white">
               LOREM IPSUM
             </h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-8 mt-12">
@@ -44,8 +44,8 @@ export default function Home() {
               </div>)}
             </div>
           </article>
-          <article className="mx-auto lg:max-w-7xl lg:pt-10 sm:pt-0 lg:mt-24 py-10 mb-[10rem] text-center">
-            <h1 className="text-4xl max-sm:mb-16 font-extrabold tracking-wider leading-none lg:text-[5.9rem] text-white uppercase">
+          <article className="container mx-auto pt-20 lg:px-4 lg:max-w-6xl lg:mt-[15rem] md:mt-[5rem]">
+            <h1 className="text-4xl max-sm:mb-16 font-extrabold tracking-wider leading-none md:text-[120px] lg:text-[5.9rem] text-white uppercase">
               lorem ipsum dolor
             </h1>
           </article>
